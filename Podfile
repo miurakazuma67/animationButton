f889055873c98d197b6654b79f1124d3677ada3c
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-05-11 15:15:04 +0900

    実装

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'animationButton' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for animationButton
+    pod 'lottie-ios'
+
+  target 'animationButtonTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'animationButtonUITests' do
+    # Pods for testing
+  end
+
+end
--- animationButton/ViewController.swift
+++ animationButton/ViewController.swift
@@ -4,16 +4,31 @@
 //
 //  Created by 三浦　一真 on 2021/05/11.
 //
-
+import Lottie
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private var animationView = AnimationView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func addAnimationView() {
+        animationView = AnimationView(name: "29219-star-smash")
+        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        animationView.isUserInteractionEnabled = true
+        view.addSubview(animationView)
+        animationView.play { finished in
+            if finished {
+                self.animationView.removeFromSuperview()
+            }
+        }
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        addAnimationView()
+    }
 }
 
